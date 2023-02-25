6ae2360f8f342b1a0922332f21b8af90ca2ec7b0
SpeedyTurtle599 <bcoveler@outlook.com>
2023-02-25 00:54:29 -0600

    ho lee fuk

---
+++ wrapper.cpp
@@ -0,0 +1,19 @@
+//
+//  wrapper.cpp
+//  cemumirror
+//
+//  Created by Benjamin Coveler on 2/25/23.
+//
+
+#include <stdio.h>
+#include "nimbase.h"
+
+// Example of importing a function from C/C++
+// https://swiftprogrammer.info/swift_call_cpp.html
+/* extern "C" int getIntFromCPP()
+{
+    // Create an instance of A, defined in
+    // the library, and call getInt() on it:
+    return A(1234).getInt();
+}
+*/
