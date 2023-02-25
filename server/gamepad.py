8659975faae64674e17f64229d8f542c3dbf6a1e
yovel <ykey-cohen@emeryweiner.org>
2023-02-25 01:27:28 -0600

    this is broken af :D

--- server/gamepad.py
+++ server/gamepad.py
@@ -1,21 +1,37 @@
 import pygetwindow as gw
-import pyautogui as gui
-import pygame
-import pygame._sdl2 as sdl2
-import os
-#import sdlgetwnd
+import sdlgetwnd
+import ctypes
+import numpy
 
 class GamePad:
     def __init__(self):
         self.window = gw.getWindowsWithTitle("GamePad View")[0]
         self.wid = self.window._hWnd
 
+    @property
+    def w(self):
+        return self.window.width
+    @property
+    def h(self):
+        return self.window.height
+    @property
+    def size(self):
+        return self.window.width, self.window.height
+
     def capture(self):
-        return gui.screenshot(region=(w.left, w.top, w.width, w.height))
+        addr = sdlgetwnd.get_img(self.wid, *self.size)
+        INTP = ctypes.POINTER(ctypes.c_int * self.w * self.h * 3)
+        ptr = ctypes.cast(addr, INTP)
+        #buffer = numpy.core.multiarray.int_asbuffer(ptr, self.w * self.h * 3 * 8)
+        return numpy.ctypeslib.as_array(ptr, shape=(self.w, self.h, 3))
+        #return ctypes.cast(, INTP)
 
 
 g = GamePad()
 print(g.wid)
+print(g.size)
+
+print(g.capture())
 
 #sdlgetwnd.test2()
 #sdl2.Window.
--- server/sdlgetwnd.nim
+++ server/sdlgetwnd.nim
@@ -1,20 +1,16 @@
 import nimpy
 import sdl2
 
-type Pixels[w, h: static[int]] =
-  array[1..w, array[1..h, array[1..3, int]]]
-
-discard sdl2.init(INIT_EVERYTHING)
-
-proc get_img(hwnd, width, height: int): Pixels[int, int] {.exportpy.} =
-  var h: ptr int
+proc get_img(hwnd, width, height: int): int {.exportpy.} =
+  var h = new int
   h[] = hwnd
-  let window = sdl2.createWindowFrom(h)
+  let window = sdl2.createWindowFrom(cast[ptr int](h))
   let renderer = sdl2.getRenderer(window)
 
-  var pixels: ptr Pixels[width, height]
+  var pixels = alloc(width*height*3)
 
   discard sdl2.readPixels(renderer, nil, 0, pixels, 0)
 
-  return pixels
+  return cast[int](pixels)
 
+echo get_img(461864, 854, 480)
